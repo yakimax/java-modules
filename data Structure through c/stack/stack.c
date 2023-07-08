7c6de5516b45f9442e87ac218ae45c7c772861df
yakimax <sv1099912@gmail.com>
2023-07-08 11:19:42 +0530

    add

--- data Structure through c/stack/stack.c
+++ data Structure through c/stack/stack.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
 #include<conio.h>
 
+int length = -1 ;
+int stack[100] ;
+
+int pop(){
+    if(length == -1){
+        printf("stack underflow") ;
+    }
+    else{
+        return stack[length--] ;
+    }
+}
+
+
+
+void peek(){
+    if(length == -1){
+        printf("stack empty") ;
+    }
+    else{
+        return stack[length--] ;
+    }
+}
+
 int main(){
     char s[100] ;
-    printf("hello my name is ");
-    gets(s);
-    printf("%s",s);
+    printf("your stack values are");
     return 0 ;
-    
 }
