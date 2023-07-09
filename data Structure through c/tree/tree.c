9f93e9fa5d9f9a19a37b2a22db6ec0f6a8adceb3
yakimax <sv1099912@gmail.com>
2023-07-09 18:21:40 +0530

    add

---
+++ data Structure through c/tree/tree.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data ;
+    struct node left ;
+    struct node right ;
+};
+
+void constructTree(int arr){
+    
+}
+
+int main(){
+    
+    return 0 ;
+}
