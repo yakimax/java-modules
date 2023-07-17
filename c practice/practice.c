e01e5302fcb9eaf5bd42968f0bcd0aca09e8a273
yakimax <sv1099912@gmail.com>
2023-07-17 21:13:08 +0530

    add

--- c practice/practice.c
+++ c practice/practice.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-
-int main(int argc, char **argv)
+int main()
 {
-    int i;
-    for(i=0; i<argc; i++){ 
-        printf("%s\n", argv[i]);
-    }
-    return 0;
+    int x = 10, y = 100%90, i;
+    for(i=1; i<10; i++)
+    if(x != y) ;
+        printf("x = %d y = %d\n", x, y);
+    return 0 ;
 }
